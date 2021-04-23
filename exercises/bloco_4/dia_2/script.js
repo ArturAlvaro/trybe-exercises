f7dbfb1ad3b446404cf8f5f25f01d904938ee653
ArturAlvaro <artur.alvaro73@gmail.com>
2021-04-23 19:20:41 -0300

    ex 5

--- exercises/bloco_4/dia_2/script.js
+++ exercises/bloco_4/dia_2/script.js
@@ -29,9 +29,20 @@ console.log(`A soma dos elementos é: ${soma} e a média dos elementos é: ${div
 
 // Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
 
-if (div > 20) {
-    console.log('Valor maior que 20!');
+// if (div > 20) {
+//     console.log('Valor maior que 20!');
+// }
+// else{
+//     console.log('Valor menor ou igual a 20!')
+// }
+
+// Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+
+let numMaior = numbers[0];
+
+for (indexMaior = 1; indexMaior < numbers.length; indexMaior += 1){
+    if (numbers[indexMaior] > numMaior){
+        numMaior = numbers[indexMaior];
+    }
 }
-else{
-    console.log('Valor menor ou igual a 20!')
-}
+console.log(`O maior número é ${numMaior}`);
