e16e01fe2f7369b10411370cd04e47bfa2a9fa51
ArturAlvaro <artur.alvaro73@gmail.com>
2021-04-23 18:37:48 -0300

    ex2

--- exercises/bloco_4/dia_2/script.js
+++ exercises/bloco_4/dia_2/script.js
@@ -2,6 +2,16 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
 
-for (index = 0; index < numbers.length; index += 1){
-    console.log(numbers[index]);
-}
+
+
+// for (index = 0; index < numbers.length; index + numbers){
+    //     console.log(numbers[index]);
+    // }
+    
+// Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+// let soma = 0;
+
+// for (index = 0; index < numbers.length; index += 1){
+// soma += numbers[index];
+// }
+// console.log(soma);
