72c5fc19a85e71c181dd5de8e0834ffda028d6cb
ArturAlvaro <artur.alvaro73@gmail.com>
2021-04-23 00:22:04 -0300

    ex 11

--- exercises/bloco_4/dia_1/script.js
+++ exercises/bloco_4/dia_1/script.js
@@ -207,3 +207,104 @@ if (valorCusto < 0 || valorVenda < 0 ){
 else{
 console.log('Ao vender mil produtos, o lucro esperado é: R$' + lucroTotal * 1000);
 };
+
+// Uma pessoa que trabalha de carteira assinada no Brasil tem descontados de seu salário bruto o INSS e o IR. Faça um programa que, dado um salário bruto, calcule o líquido a ser recebido.
+// A notação para um salário de R$1500,10, por exemplo, deve ser 1500.10. Para as faixas de impostos, use as seguintes referências:
+// INSS (Instituto Nacional do Seguro Social)
+
+// IR (Imposto de Renda)
+// Até R$ 1.903,98: isento de imposto de renda
+// De R$ 1.903,99 a 2.826,65: alíquota de 7,5% e parcela de R$ 142,80 a deduzir do imposto
+// De R$ 2.826,66 a R$ 3.751,05: alíquota de 15% e parcela de R$ 354,80 a deduzir do imposto
+// De R$ 3.751,06 a R$ 4.664,68: alíquota de 22,5% e parcela de R$ 636,13 a deduzir do imposto
+// Acima de R$ 4.664,68: alíquota de 27,5% e parcela de R$ 869,36 a deduzir do imposto.
+// Exemplo : Uma pessoa possui o salário bruto de R$ 3.000,00. O cálculo será:
+// O salário bruto está entre R$ 2.594,93 e R$ 5.189,82, então sua alíquota para INSS é de 11%. O INSS será 11% de R$ 3.000, ou seja, R$ 330,00.
+// Para descobrir o salário-base, subtraia do salário bruto a alíquota do INSS: R$ 3.000,00 - R$ 330,00 = R$ 2.670,00.
+// Para pegar o valor do IR, temos um salário (já deduzido o INSS) entre R$ 1.903,99 e 2.826,65, sendo a alíquota, então, de 7.5%, com parcela de R$ 142,80 a deduzir do imposto. Assim, temos:
+// R$ 2.670,00: salário com INSS já deduzido;
+// 7.5%: alíquota de imposto de renda;
+// R$ 142,80 parcela a se deduzir do imposto.
+// Fazendo a conta, temos: (7,5% de R$ 2.670,00) - R$ 142,80 = R$ 57,45
+// O último cálculo para conseguir o salário líquido é R$ 2.670,00 - R$ 57,45 (salário-base - valor IR) = R$ 2.612,55.
+// Resultado: R$ 2.612,55.
+
+
+// Salário bruto até R$ 1.556,94: alíquota de 8%
+// Salário bruto de R$ 1.556,95 a R$ 2.594,92: alíquota de 9%
+// Salário bruto de R$ 2.594,93 a R$ 5.189,82: alíquota de 11%
+// Salário bruto acima de R$ 5.189,82: alíquota máxima de R$ 570,88
+
+let salarioBruto = 5189.83;
+let ir;
+
+if (salarioBruto < 0) {
+    console.log('valor inválido!');
+}
+else if (salarioBruto >= 0 && salarioBruto <= 1556.94) {
+    let inss = (salarioBruto / 100) * 8;
+    let salarioBase = salarioBruto - inss;
+    console.log('Seu salário bruto é: R$ ' + salarioBruto.toFixed(2) + '. O INSS é: R$ ' + inss.toFixed(2) + '. Seu salário base é: R$' + salarioBase.toFixed(2));
+
+        if (salarioBase <= 1903.98) {
+            console.log('Isento de Imposto de Renda, seu salário líquido é: R$' + salarioBase.toFixed(2));
+        }
+        else if (salarioBase >= 1903.99 && salarioBase <= 2826.65) {
+            let novoSalarioBase = (salarioBase / 100) * 7.5 - 142.80;
+            let salarioLiquido = salarioBase - novoSalarioBase;
+            console.log('Sua alíquota de IR é: 7.5% e a parcela a deduzir no imposto é: R$ 142.80. Seu salário líquido é: R$ ' + salarioLiquido.toFixed(2));
+        }
+
+} else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
+    let inss = (salarioBruto / 100) * 9;
+    let salarioBase = salarioBruto - inss;
+    console.log('Seu salário bruto é: R$ ' + salarioBruto.toFixed(2) + '. O INSS é: R$ ' + inss.toFixed(2) + '. Seu salário base é: R$' + salarioBase.toFixed(2));
+
+        if (salarioBase <= 1903.98) {
+            console.log('Isento de Imposto de Renda, seu salário líquido é: R$' + salarioBase.toFixed(2));
+        }
+        else if (salarioBase >= 1903.99 && salarioBase <= 2826.65) {
+            let novoSalarioBase = (salarioBase / 100) * 7.5 - 142.80;
+            let salarioLiquido = salarioBase - novoSalarioBase;
+            console.log('Sua alíquota de IR é: 7.5% e a parcela a deduzir no imposto é: R$ 142.80. Seu salário líquido é: R$ ' + salarioLiquido.toFixed(2));
+        }
+
+} else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82) {
+    let inss = (salarioBruto / 100) * 11;
+    let salarioBase = salarioBruto - inss;
+    console.log('Seu salário bruto é: R$ ' + salarioBruto.toFixed(2) + '. O INSS é: R$ ' + inss.toFixed(2) + '. Seu salário base é: R$' + salarioBase.toFixed(2));
+
+        if (salarioBase <= 1903.98) {
+            console.log('Isento de Imposto de Renda, seu salário líquido é: R$' + salarioBase.toFixed(2));
+        }
+        else if (salarioBase >= 1903.99 && salarioBase <= 2826.65) {
+            let novoSalarioBase = (salarioBase / 100) * 7.5 - 142.80;
+            let salarioLiquido = salarioBase - novoSalarioBase;
+            console.log('Sua alíquota de IR é: 7.5% e a parcela a deduzir no imposto é: R$ 142.80. Seu salário líquido é: R$ ' + salarioLiquido.toFixed(2));
+        } else if (salarioBase >= 2826.66 && salarioBase <= 3751.05) {
+            let novoSalarioBase = (salarioBase / 100) * 15 - 354.80;
+            let salarioLiquido = salarioBase - novoSalarioBase;
+            console.log('Sua alíquota de IR é: 15% e a parcela a deduzir no imposto é: R$ 354.80. Seu salário líquido é: R$ ' + salarioLiquido.toFixed(2));
+        }
+        else if (salarioBase >= 3751.06 && salarioBase <= 4664.68) {
+            let novoSalarioBase = (salarioBase / 100) * 27.5 - 869.36;
+            let salarioLiquido = salarioBase - novoSalarioBase;
+            console.log('Sua alíquota de IR é: 27.5% e a parcela a deduzir no imposto é: R$ 869.36. Seu salário líquido é: R$ ' + salarioLiquido.toFixed(2));}
+
+} else if (salarioBruto >= 5189.83) {
+    let inss = 570.88;
+    let salarioBase = salarioBruto - inss;
+    console.log('Seu salário bruto é: R$ ' + salarioBruto.toFixed(2) + '. O INSS é: R$ ' + inss.toFixed(2) + '. Seu salário base é: R$' + salarioBase.toFixed(2));
+
+    if (salarioBase >= 3751.06 && salarioBase <= 4664.68) {
+        let novoSalarioBase = (salarioBase / 100) * 22.5 - 636.13;
+        let salarioLiquido = salarioBase - novoSalarioBase;
+        console.log('Sua alíquota de IR é: 22.5% e a parcela a deduzir no imposto é: R$ 636.13. Seu salário líquido é: R$ ' + salarioLiquido.toFixed(2));
+    }
+    else if (salarioBase >= 4664.68) {
+        let novoSalarioBase = (salarioBase / 100) * 27.5 - 869.36;
+        let salarioLiquido = salarioBase - novoSalarioBase;
+        console.log('Sua alíquota de IR é: 27.5% e a parcela a deduzir no imposto é: R$ 869.36. Seu salário líquido é: R$ ' + salarioLiquido.toFixed(2));
+    }
+
+}
