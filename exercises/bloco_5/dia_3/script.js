091d9adddd7fd2cf1ec380daffb3d06abedbd8a4
ArturAlvaro <artur.alvaro73@gmail.com>
2021-05-04 19:39:03 -0300

    exercicio 1, parte 1

--- exercises/bloco_5/dia_3/script.js
+++ exercises/bloco_5/dia_3/script.js
@@ -9,8 +9,26 @@ function createDaysOfTheWeek() {
   
       weekDaysList.appendChild(dayListItem);
     };
-  };
+};
   
   createDaysOfTheWeek();
-  
-  // Escreva seu código abaixo.
+
+
+// dezDaysList deve ser <li> e com classe 'day'. Ex: <li class="da">3</li>
+// dias 24, 25  e 31 são feriados e, além da classe 'day', devem conter também classe 'holiday'. 
+// Ex:<li class="day holiday">24</li>
+// dias 4, 11, 18 e 25 são sexta-feira e devem conter classe 'day' e classe 'friday'. Ex: <li class="day friday">4</li>
+
+function dezDaysFunction() {
+    const dezDays = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+    const dezDaysList = document.getElementById('days');
+
+    for (let index = 0; index < dezDays.length; index += 1) {
+        const dias = dezDays[index];
+        const daysListItems = document.createElement('li');
+        daysListItems.innerHTML = dias;
+
+        dezDaysList.appendChild(daysListItems);
+    };
+};
+dezDaysFunction();
