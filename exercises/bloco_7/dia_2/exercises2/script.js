7b36ab06853266a0e8a280bd58e43c26ed569cd5
ArturAlvaro <artur.alvaro73@gmail.com>
2021-05-19 18:54:03 -0300

    Parte II Exercicio 6

--- exercises/bloco_7/dia_2/exercises2/script.js
+++ exercises/bloco_7/dia_2/exercises2/script.js
@@ -20,31 +20,31 @@ const lesson3 = {
 
 // Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
 
-// const addLessonitem = (value) => {
-//   lesson2['turno'] = 'manhã';
-//   return Object.entries(value);
-// };
-// console.log(addLessonitem(lesson2));
+const addLessonitem = (value) => {
+  lesson2['turno'] = 'manhã';
+  return Object.entries(value);
+};
+console.log(addLessonitem(lesson2));
 
-// // Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+// Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
 
-// const keyLesson = (value) => {
-//   return Object.keys(value);
-// };
-// console.log(keyLesson(lesson3));
-// // Crie uma função para mostrar o tamanho de um objeto.
+const keyLesson = (value) => {
+  return Object.keys(value);
+};
+console.log(keyLesson(lesson3));
+// Crie uma função para mostrar o tamanho de um objeto.
 
-// const sizeObj = (value) => {
-//   return Object.keys(value).length;
-// };
-// console.log(sizeObj(lesson3));
+const sizeObj = (value) => {
+  return Object.keys(value).length;
+};
+console.log(sizeObj(lesson3));
 
-// //Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
+//Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
 
-// const listObj = (value) => {
-//   return Object.values(value);
-// };
-// console.log(listObj(lesson3));
+const listObj = (value) => {
+  return Object.values(value);
+};
+console.log(listObj(lesson3));
 
 // Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . Ao executar o comando console.log(allLessons) , a saída deverá ser a seguinte:
 
@@ -69,3 +69,13 @@ console.log(allLessons);
      turno: 'noite' }
 };
 */
+
+// Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
+
+const totalStudents = (allLessons) => {
+  const stdnts1 = allLessons.lesson1.numeroEstudantes;
+  const stdnts2 = allLessons.lesson2.numeroEstudantes;
+  const stdnts3 = allLessons.lesson3.numeroEstudantes;
+  return `O número total de estudantes é: ${stdnts1 + stdnts2 + stdnts3}!`;
+};
+console.log(totalStudents(allLessons));
