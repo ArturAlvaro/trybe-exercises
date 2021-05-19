80d9169aff880285fb7424c9461a24ce03313597
ArturAlvaro <artur.alvaro73@gmail.com>
2021-05-19 18:28:00 -0300

    Parte II Exercicio 5

--- exercises/bloco_7/dia_2/exercises2/script.js
+++ exercises/bloco_7/dia_2/exercises2/script.js
@@ -20,28 +20,52 @@ const lesson3 = {
 
 // Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
 
-const addLessonitem = (value) => {
-  lesson2['turno'] = 'manhã';
-  return Object.entries(value);
+// const addLessonitem = (value) => {
+//   lesson2['turno'] = 'manhã';
+//   return Object.entries(value);
+// };
+// console.log(addLessonitem(lesson2));
+
+// // Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+
+// const keyLesson = (value) => {
+//   return Object.keys(value);
+// };
+// console.log(keyLesson(lesson3));
+// // Crie uma função para mostrar o tamanho de um objeto.
+
+// const sizeObj = (value) => {
+//   return Object.keys(value).length;
+// };
+// console.log(sizeObj(lesson3));
+
+// //Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
+
+// const listObj = (value) => {
+//   return Object.values(value);
+// };
+// console.log(listObj(lesson3));
+
+// Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . Ao executar o comando console.log(allLessons) , a saída deverá ser a seguinte:
+
+const allLessons = Object.assign({}, { lesson1, lesson2, lesson3 });
+console.log(allLessons);
+/*
+{
+  lesson1:
+   { materia: 'Matemática',
+     numeroEstudantes: 20,
+     professor: 'Maria Clara',
+     turno: 'manhã' },
+  lesson2:
+   { materia: 'História',
+     numeroEstudantes: 20,
+     professor: 'Carlos',
+     turno: 'noite' },
+  lesson3:
+   { materia: 'Matemática',
+     numeroEstudantes: 10,
+     professor: 'Maria Clara',
+     turno: 'noite' }
 };
-console.log(addLessonitem(lesson2));
-
-// Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
-
-const keyLesson = (value) => {
-  return Object.keys(value);
-};
-console.log(keyLesson(lesson3));
-// Crie uma função para mostrar o tamanho de um objeto.
-
-const sizeObj = (value) => {
-  return Object.keys(value).length;
-};
-console.log(sizeObj(lesson3));
-
-//Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
-
-const listObj = (value) => {
-  return Object.values(value);
-};
-console.log(listObj(lesson3));
+*/
