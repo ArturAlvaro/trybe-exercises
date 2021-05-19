51072de21c5889eb8648421562c80deac61a4285
ArturAlvaro <artur.alvaro73@gmail.com>
2021-05-19 16:45:27 -0300

    Parte I exercicio 3

---
+++ exercises/bloco_7/dia_2/exercises2/script.js
@@ -0,0 +1,40 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+// Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+
+const addLessonitem = (value) => {
+  lesson2['turno'] = 'manhã';
+  return Object.entries(value);
+};
+console.log(addLessonitem(lesson2));
+
+// Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+
+const keyLesson = (value) => {
+  return Object.keys(value);
+};
+console.log(keyLesson(lesson3));
+// Crie uma função para mostrar o tamanho de um objeto.
+
+const sizeObj = (value) => {
+  return Object.keys(value).length;
+};
+console.log(sizeObj(lesson3));
