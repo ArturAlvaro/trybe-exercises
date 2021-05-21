174c99363bb5cd181048a5884d01046906672ba2
ArturAlvaro <artur.alvaro73@gmail.com>
2021-05-20 21:07:26 -0300

    Testes Unitarios pt2, exercicio 4

---
+++ exercises/bloco_7/dia_3/exercises/script9.js
@@ -0,0 +1,19 @@
+const assert = require('assert');
+// escreva a função findTheNeedle aqui
+
+let words = ['house', 'train', 'slide', 'needle', 'book'];
+words = ['plant', 'shelf', 'arrow', 'bird'];
+
+const findTheNeedle = (value, string) => {
+  if (value.includes(string)) {
+    return value.indexOf(string);
+  }
+  return -1;
+};
+let expected = 1;
+let output = findTheNeedle(words, 'shelf');
+assert.strictEqual(output, expected);
+
+// expected = -1;
+// output = findTheNeedle(words, 'plat');
+// assert.strictEqual(output, expected);
