7dcfa6e80b82f6908275bbcad7a9c67bdaf31bf8
ArturAlvaro <artur.alvaro73@gmail.com>
2021-05-26 18:21:44 -0300

    Conteudo aula, bloco 8

---
+++ exercises/bloco_8/dia_2/conteudo_aula/script.js
@@ -0,0 +1,84 @@
+// item, index, obj todo
+const arraOfValues = ['José', 50, 0.25, { comida: 'Chocolate' }];
+arraOfValues.forEach((xablau0, xablau1, xablau2) => {
+  console.log('Cada elemento do array:', xablau0);
+  console.log('Index, posição do elemento:', xablau1);
+  console.log('Arry percorrido:', xablau2);
+});
+
+// nomes para upper case
+const names = ['Bianca', 'Camila', 'Fernando', 'Ana Roberta'];
+
+const convertToUpperCase = (name, index) => {
+  names[index] = name.toUpperCase();
+};
+
+names.forEach(convertToUpperCase);
+console.log(names); // [ 'BIANCA', 'CAMILA', 'FERNANDO', 'ANA ROBERTA' ]
+
+// Use o método forEach chamando a callback showEmailList para apresentar os emails
+const emailListInData = [
+  'roberta@email.com',
+  'paulo@email.com',
+  'anaroberta@email.com',
+  'fabiano@email.com',
+];
+
+const showEmailList = (email) => {
+  console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
+};
+
+// Adicione seu código aqui
+emailListInData.forEach(showEmailList);
+
+// outros exemplos
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const verifyEven = (number) => number % 2 === 0;
+
+const isEven = numbers.find(verifyEven);
+
+console.log(isEven); // 30
+
+console.log(verifyEven(9)); // False
+console.log(verifyEven(14)); // True
+
+// Outra forma de ser realizada sem a necessidade de criar uma nova função.
+const isEven2 = numbers.find((number) => number % 2 === 0);
+
+console.log(isEven2); // 30
+
+// PARA FIXAR
+// Utilize find para retornar o primeiro número que é divisível por 3 e 5, caso ele exista:
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = () => {
+  // Adiciona seu código aqui
+  return numbers.find((element) => element % 3 === 0 && element % 5 === 0);
+};
+
+console.log(findDivisibleBy3And5());
+
+// Utilize o find para encontrar o primeiro nome com cinco letras, caso ele exista:
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const findNameWithFiveLetters = () => {
+  // Adicione seu código aqui:
+  return names.find((name) => name.length === 5);
+};
+
+console.log(findNameWithFiveLetters());
+
+// Utilize o find para encontrar a música com id igual a 31031685 , caso ela exista:
+const musicas = [
+  { id: '31031685', title: 'Partita in C moll BWV 997' },
+  { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+  { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+];
+
+function findMusic(id) {
+  // Adicione seu código aqui
+  return musicas.find((musica) => musica.id === '31031685');
+}
+
+console.log(findMusic('31031685'));
