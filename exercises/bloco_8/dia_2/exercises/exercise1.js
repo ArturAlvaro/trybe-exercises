416ae332ef7178fafd5222de68e73623ef7f6a6e
ArturAlvaro <artur.alvaro73@gmail.com>
2021-05-26 23:01:12 -0300

    Bloco 8, exercicio 2

--- exercises/bloco_8/dia_2/exercises/exercise1.js
+++ exercises/bloco_8/dia_2/exercises/exercise1.js
@@ -73,3 +73,19 @@ function authorBornIn1947() {
 // console.log(authorBornIn1947());
 
 assert.strictEqual(authorBornIn1947(), 'Stephen King');
+
+// Retorne o nome do livro de menor nome
+function smallerName() {
+  let nameBook;
+  // escreva aqui o seu código
+  books.forEach((book) => {
+    if (!nameBook || book.name.length < nameBook.length) {
+      nameBook = book.name;
+    }
+  });
+
+  // Variável nameBook que receberá o valor do menor nome;
+  return nameBook;
+}
+
+assert.strictEqual(smallerName(), 'Duna');
