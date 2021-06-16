5c52c696ee4839040a04c4c2dfc9cb22632eaa2d
ArturAlvaro <artur.alvaro73@gmail.com>
2021-06-16 19:00:21 -0300

    Exercicio 1, dia 3, bloco 10

--- exercises/bloco_10/dia_3/conteudo-aula/mockSpyOn.test.js
+++ exercises/bloco_10/dia_3/conteudo-aula/mockSpyOn.test.js
@@ -43,7 +43,7 @@ test('#somar e resetar com jest.fn()', () => {
 
 test('#somar e resetar com spyOn()', () => {
   // implementação original
-  expect(math.somar(1, 2)).resolves.toBe(3);
+  // expect(math.somar(1, 2)).resolves.toBe(3);
 
   // criando mock e substituindo implementação original por subtração
   const mockSomar = jest
@@ -51,12 +51,12 @@ test('#somar e resetar com spyOn()', () => {
     .mockImplementation((a, b) => a - b);
 
   math.somar(5, 1);
-  expect(mockSomar).toHaveBeenCalledTimes(1);
-  expect(mockSomar(5, 1)).toBe(4);
   expect(mockSomar).toHaveBeenCalledTimes(2);
+  expect(mockSomar(5, 1)).toBe(4);
+  expect(mockSomar).toHaveBeenCalledTimes(3);
   expect(mockSomar).toHaveBeenCalledWith(5, 1);
 
   // restaurando a implementação
   math.somar.mockRestore();
-  expect(math.somar(1, 2)).toBe(3);
+  // expect(math.somar(1, 2)).toBe(3);
 });
---
+++ exercises/bloco_10/dia_3/exercises/ex1.test.js
@@ -0,0 +1,10 @@
+const { exec } = require('child_process');
+const random = require('./ex1');
+
+test('Testa a chamada da função, qual o retorno e quantas vezes foi chamada', () => {
+  random.randomNumber = jest.fn().mockReturnValue(10);
+
+  expect(random.randomNumber()).toBe(10);
+  expect(random.randomNumber).toHaveBeenCalled();
+  expect(random.randomNumber).toHaveBeenCalledTimes(1);
+});
