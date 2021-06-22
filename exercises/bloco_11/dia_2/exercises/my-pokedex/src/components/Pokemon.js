c32a9f3c0b73a60680d6fe5cbaa0deed9d1c5ac5
ArturAlvaro <artur.alvaro73@gmail.com>
2021-06-22 19:29:02 -0300

    my pokedex

---
+++ exercises/bloco_11/dia_2/exercises/my-pokedex/src/components/Pokemon.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Pokemon extends React.Component {
+  render() {
+    const { name, type, averageWeight, image } = this.props.pokemon;
+    return (
+      <div className='pokemon'>
+        <div>
+          <p>{name}</p>
+          <p>{type}</p>
+          <p>{`The average weight is ${averageWeight.value} ${averageWeight.measurementUnit}`}</p>
+        </div>
+        <img src={image} alt={`${name} sprite`} />
+      </div>
+    );
+  }
+}
+
+Pokemon.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.string,
+    type: PropTypes.string,
+    averageWeight: PropTypes.shape({
+      measurementUnit: PropTypes.string,
+      value: PropTypes.number,
+    }),
+    image: PropTypes.string,
+  }).isRequired,
+};
+
+export default Pokemon;
