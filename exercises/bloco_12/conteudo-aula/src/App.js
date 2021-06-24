c3ecc65e6fdf118ef4211b4fe209e1044ca00c2a
ArturAlvaro <artur.alvaro73@gmail.com>
2021-06-24 14:00:44 -0300

    Eventos e estados

---
+++ exercises/bloco_12/conteudo-aula/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor() {
+    super()
+    // a função abaixo vincula 'manualmente' o this à nossa função
+    this.state = {
+      numeroDeCliques: 0
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = () => {
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques: estadoAnterior.numeroDeCliques + 1
+    }))
+  }
+
+  render() {
+    return <button onClick={this.handleClick}>{this.state.numeroDeCliques}</button>
+  }
+}
+
+export default App;
