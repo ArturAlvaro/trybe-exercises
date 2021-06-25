7c38cc87f3d1de1dc3b347d3c1614fc51475d581
ArturAlvaro <artur.alvaro73@gmail.com>
2021-06-25 11:20:31 -0300

    Event handlers genericos

--- exercises/bloco_12/dia_2/conteudo-aula/src/Form.js
+++ exercises/bloco_12/dia_2/conteudo-aula/src/Form.js
@@ -5,53 +5,64 @@ class Form extends React.Component {
     super()
 
     this.handleChange = this.handleChange.bind(this)
-    this.feedback = this.feedback.bind(this)
+    this.fileInput = React.createRef()
 
     this.state = {
       estadoFavorito: '',
+      idade: 0,
+      vaiComparecer: false,
+      filmeFavorito: '',
+      regiao: '',
+      numeroDaSorte: '',
       feedback: '',
     }
   }
 
-  feedback(event) {
+  handleChange({ target }) {
+    const { name } = target
+    const value = target.type === 'checkbox' ? target.checked : target.value
     this.setState({
-      feedback: event.target.value,
-    })
-  }
-
-  handleChange(event) {
-    this.setState({
-      estadoFavorito: event.target.value,
+      [name]: value
     })
   }
   render() {
     return (
       <div>
         <h1>Estados e React - Tecnologia fantástica ou reagindo a regionalismos?</h1>
-        <form className="form">
-          <label>Diga qual o seu Estado favorito! De React ou do Brasil, você decide!
-            <textarea name='estadoFavorito' value={this.state.estadoFavorito} onChange={this.handleChange} />
-          </label>
+        <fieldset>
+          <form className="form">
+            <label>Diga qual o seu Estado favorito! De React ou do Brasil, você decide!
+              <textarea name="estadoFavorito" value={this.state.estadoFavorito} onChange={this.handleChange} />
+            </label>
+          </form>
+        </fieldset>
+        <fieldset>
           <label>Idade:
-          <input type="number" name="idade" />
+          <input type="number" name="idade" value={this.state.idade} onChange={this.handleChange} />
           </label>
-          <label>Vai Comparecer?
-          <input type="checkbox" name="vaiComparecer" />
+          <label>Filme Favorito:
+          <input type="text" name="filmeFavorito" value={this.state.filmeFavorito} onChange={this.handleChange} />
           </label>
-          <label>
-          <input type="text" name="filmeFavorito" />
+          <label>Número da Sorte:
+          <input type="number" name="numeroDaSorte" value={this.state.numeroDaSorte} onChange={this.handleChange} />
           </label>
-          <label>
-          <input type="number" name="numeroDaSorte" />
-          </label>
-          <select>
-            <option>Capital</option>
-            <option>Interior</option>
-          </select>
-          <label>Feedback:
-            <textarea name="feedback" value={this.state.feedback} onChange={this.feedback} />
-          </label>
-        </form>
+        </fieldset>
+        <fieldset>
+            <label>Vai Comparecer?
+            <input type="checkbox" name="vaiComparecer" value={this.state.vaiComparecer} onChange={this.handleChange} />
+            </label>
+            <select name="regiao" value={this.state.regiao} onChange={this.handleChange}>
+              <option value="capital">Capital</option>
+              <option value="interior">Interior</option>
+            </select>
+            <label>Feedback:
+              <textarea name="feedback" value={this.state.feedback} onChange={this.handleChange} />
+            </label>
+            <label>Abrir arquivo:
+              <input type="file" ref={this.fileInput} />
+            </label>
+        </fieldset>
+
       </div>
     )
   }
