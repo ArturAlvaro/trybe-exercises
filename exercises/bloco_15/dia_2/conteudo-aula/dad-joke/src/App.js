c2bc4425921c0f62194017064f59f21f8f551ff2
ArturAlvaro <artur.alvaro73@gmail.com>
2021-07-19 11:33:16 -0300

    Dia 2, Testando inputs em React

--- exercises/bloco_15/dia_2/conteudo-aula/dad-joke/src/App.js
+++ exercises/bloco_15/dia_2/conteudo-aula/dad-joke/src/App.js
@@ -5,31 +5,59 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      joke: '',
-    }
+      joke: "",
+      nome: "",
+      email: "",
+    };
+  }
+
+  handleInput(e) {
+    const { name, value } = e.target;
+    console.log(name, value);
+    this.setState({ [name]: value });
   }
 
   componentDidMount() {
-    const API_URL = 'https://icanhazdadjoke.com/';
-    fetch(API_URL, { headers: { Accept: 'application/json' } })
-      .then((response) => response.json()
-      .then((data) => this.setState({
-        joke: data.joke,
-      })
-      ))
+    const API_URL = "https://icanhazdadjoke.com/";
+    fetch(API_URL, { headers: { Accept: "application/json" } }).then(
+      (response) =>
+        response.json().then((data) =>
+          this.setState({
+            joke: data.joke,
+          }),
+        ),
+    );
   }
 
   render() {
     const { joke } = this.state;
     return (
-      <div className="App">
-        { joke }
+      <div>
+        <div className="App">{joke}</div>
+        <div>
+          <h1>Teste de inputs</h1>
+          <p>
+            Nome:
+            <input
+              onChange={(e) => this.handleInput(e)}
+              name="nome"
+              value={this.state.nome}
+              data-testid="input-nome"
+            />
+          </p>
+          <p>
+            E-mail:
+            <input
+              onChange={(e) => this.handleInput(e)}
+              name="email"
+              value={this.state.email}
+              data-testid="input-email"
+            />
+          </p>
+        </div>
       </div>
     );
   }
 }
 
 export default App;
-
-// 'https://icanhazdadjoke.com/'
-// { headers: { Accept: 'application/json' } }
--- exercises/bloco_15/dia_2/conteudo-aula/dad-joke/src/App.test.js
+++ exercises/bloco_15/dia_2/conteudo-aula/dad-joke/src/App.test.js
@@ -2,11 +2,32 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-describe('Testa resposta da API dadJoke', () => {
+afterEach(() => jest.clearAllMocks());
+it("fetch joke", async () => {
+  const joke = {
+    id: "7h3oGtr0fxc",
+    joke: "How the turntables... turn",
+    status: 200,
+  };
+
+  jest.spyOn(global, "fetch");
+  global.fetch.mockResolvedValue({
+    json: jest.fn().mockResolvedValue(joke),
+  });
+
+  const { findByText } = render(<App />);
+  await findByText("How the turntables... turn");
+  expect(global.fetch).toBeCalledTimes(1);
+  expect(global.fetch).toBeCalledWith("https://icanhazdadjoke.com/", {
+    headers: { Accept: "application/json" },
+  });
+});
+
+describe("Testa resposta da API dadJoke", () => {
   it('Testa se retorna a resposta: "How the turntables... turn"', async () => {
     const joke = {
-      id: '7h3oGtr0fxc',
-      joke: 'How the turntables... turn',
+      id: "7h3oGtr0fxc",
+      joke: "How the turntables... turn",
       status: 200,
     };
 
@@ -15,7 +36,6 @@ describe('Testa resposta da API dadJoke', () => {
     });
 
     const { findByText } = render(<App />);
-    await findByText('How the turntables... turn');
-  })
+    await findByText("How the turntables... turn");
+  });
 });
-
