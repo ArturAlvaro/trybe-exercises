5301d7b4e49fb22833dacdcf0a37324faa57a117
ArturAlvaro <artur.alvaro73@gmail.com>
2021-09-17 15:32:28 -0300

    Unions

---
+++ exercises/bloco_21/dia_2/conteudoAula/union/ex2.sql
@@ -0,0 +1,11 @@
+(SELECT 
+    first_name, last_name
+FROM
+    sakila.customer
+WHERE
+    first_name LIKE '%tracy%') UNION (SELECT 
+    first_name, last_name
+FROM
+    sakila.actor
+WHERE
+    first_name LIKE '%je%');
