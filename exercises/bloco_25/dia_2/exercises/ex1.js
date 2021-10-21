2d54ce4988f3facd918fec5f662fee1907eb3521
ArturAlvaro <artur.alvaro73@gmail.com>
2021-10-21 15:39:55 -0300

    Dia 2, exercicio 1

---
+++ exercises/bloco_25/dia_2/exercises/ex1.js
@@ -0,0 +1,31 @@
+const checkNumber = (num1, num2, num3) => {
+  return new Promise((resolve, reject) => {
+    if (
+      typeof num1 !== 'number' ||
+      typeof num2 !== 'number' ||
+      typeof num3 !== 'number'
+    ) {
+      reject('Informe apenas números!');
+    }
+
+    const result = (num1 + num2) * num3;
+
+    if (result < 50) {
+      reject('Valor muito baixo');
+    };
+
+    resolve(result);
+  });
+};
+
+checkNumber(10, 14, 41)
+  .then(resolve => console.log(resolve))
+  .catch(err => console.log(err));
+
+checkNumber(10, 14, '41')
+  .then(resolve => console.log(resolve))
+  .catch(err => console.log(err));
+
+checkNumber(1, 1, 1)
+  .then(resolve => console.log(resolve))
+  .catch(err => console.log(err));
