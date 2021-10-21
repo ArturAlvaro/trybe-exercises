c790f7f586e21e20ab96d40e251c829d824af087
ArturAlvaro <artur.alvaro73@gmail.com>
2021-10-21 08:43:27 -0300

    Velocidade refatorado!

--- exercises/bloco_25/dia_1/velocidade/index.js
+++ exercises/bloco_25/dia_1/velocidade/index.js
@@ -1,3 +1,16 @@
 const velocidade = require('./velocidade');
+const readLine = require('readline-sync');
 
-velocidade();
+const askDistance = () => {
+  return readLine.questionInt('Distância percorrida: ');
+};
+
+const askTime = () => {
+  return readLine.questionInt('Tempo gasto: ');
+};
+
+const main = () => {
+  console.log(`Sua velocidade é ${velocidade(askDistance(), askTime())} m/s!`);
+};
+
+main();
--- exercises/bloco_25/dia_1/velocidade/velocidade.js
+++ exercises/bloco_25/dia_1/velocidade/velocidade.js
@@ -1,11 +1,5 @@
-const readLine = require('readline-sync');
-
-const velocidade = () => {
-  const distancia = readLine.questionInt('Distância percorrida: ');
-  const tempo = readLine.questionInt('Tempo gasto: ');
-  const result = (distancia / tempo).toFixed(2);
-
-  console.log(`Sua distância é ${distancia}, seu tempo é ${tempo}. Sua velocidade é ${result} m/s!`);
+const velocidade = (distancia, tempo) => {
+  return (distancia / tempo).toFixed(2);
 };
 
 module.exports = velocidade;
