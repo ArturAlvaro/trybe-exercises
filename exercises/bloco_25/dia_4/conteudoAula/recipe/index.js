14b07d72ac8b9098ff837b5dc33ad2303e7cedc2
ArturAlvaro <artur.alvaro73@gmail.com>
2021-10-25 11:23:11 -0300

    Adicionando parametro de rota

--- exercises/bloco_25/dia_4/conteudoAula/recipe/index.js
+++ exercises/bloco_25/dia_4/conteudoAula/recipe/index.js
@@ -7,8 +7,13 @@ const recipes = [
   { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
 ];
 
-app.get('/recipes', function (_req, res) {
-  res.json(recipes);
+app.get('/recipes/:id', function (req, res) {
+  const { id } = req.params;
+  const recipe = recipes.find((r) => r.id === parseInt(id));
+
+  if (!recipe) return res.status(404).json({ message: 'Recipe not found' });
+
+  res.status(200).json(recipe);
 });
 
 app.listen(3001, () => {
