ad2e6f7df9b7dcd65e50c5a467ce16af7c443cec
ArturAlvaro <artur.alvaro73@gmail.com>
2021-10-25 11:51:54 -0300

    Query String

--- exercises/bloco_25/dia_4/conteudoAula/recipe/index.js
+++ exercises/bloco_25/dia_4/conteudoAula/recipe/index.js
@@ -3,9 +3,17 @@ const app = express();
 
 const recipes = [
   { id: 1, name: 'Lasanha', price: 40.0, waitTime: 30 },
-  { id: 2, name: 'Macarrão a Bolonhesa', price: 35.0, waitTime: 25 },
-  { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
+  { id: 2, name: 'Macarrao a Bolonhesa', price: 35.0, waitTime: 25 },
+  { id: 3, name: 'Macarrao com molho branco', price: 35.0, waitTime: 25 },
 ];
+
+app.get('/recipes/search', function(req, res) {
+  const { name, maxPrice } = req.query;
+  const filteredRecipe = recipes.filter(
+    (r) => r.name.includes(name) && r.price < parseInt(maxPrice),
+  );
+  res.status(200).json(filteredRecipe);
+});
 
 app.get('/recipes/:id', function (req, res) {
   const { id } = req.params;
