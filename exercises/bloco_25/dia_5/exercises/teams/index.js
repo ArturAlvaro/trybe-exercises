5191a9273d222e30bf7c6b4d9970e9b4ca19c381
ArturAlvaro <artur.alvaro73@gmail.com>
2021-10-26 18:37:50 -0300

    Teams finalizado

--- exercises/bloco_25/dia_5/exercises/teams/index.js
+++ exercises/bloco_25/dia_5/exercises/teams/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const fs = require('fs/promises');
 const router = require('./routers');
+const errorMid = require('./middlewares/errorMiddleware');
+
 const app = express();
 app.use(express.json());
 
 app.use('/teams', router);
+app.use(errorMid);
 
 app.listen(3001, () => {
   console.log('Listening on port 3001');
--- exercises/bloco_25/dia_5/exercises/teams/routers.js
+++ exercises/bloco_25/dia_5/exercises/teams/routers.js
@@ -1,17 +1,21 @@
 const router = require('express').Router();
 const fs = require('fs/promises');
+const rescue = require('express-rescue');
 
 const { validCountry, validInitials, validName } = require('./middlewares/authMiddleware');
 
-router.get('/', async (_req, res) => {
-  const teams = JSON.parse(await fs.readFile('./teams.json', 'utf8'));
-  
-  return res.status(200).json({ teams, });
-});
+router.get('/', [
+  rescue(async (_req, res) => {
+    const teams = JSON.parse(await fs.readFile("./teams.json", "utf8"));
+
+    return res.status(200).json({ teams, });
+  }),
+]);
 
 router.use([validCountry, validInitials, validName]);
 
-router.post('/', async (req, res) => {
+router.post('/', [
+  rescue(async (req, res) => {
   const { name, initials, country, league } = req.body;
   const teams = JSON.parse(await fs.readFile('./teams.json', 'utf8'));
 
@@ -20,6 +24,6 @@ router.post('/', async (req, res) => {
   await fs.writeFile('./teams.json', JSON.stringify(teams));
 
   return res.status(200).json({ message: newTeam });
-});
+})]);
 
 module.exports = router;
