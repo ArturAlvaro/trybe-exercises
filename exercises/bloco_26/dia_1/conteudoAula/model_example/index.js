bc78d1c54fb481f894f2c03d6ed73c8cce091bb7
ArturAlvaro <artur.alvaro73@gmail.com>
2021-11-13 14:42:48 -0300

    Node e MySQL, buscando por id

--- exercises/bloco_26/dia_1/conteudoAula/model_example/index.js
+++ exercises/bloco_26/dia_1/conteudoAula/model_example/index.js
@@ -8,7 +8,17 @@ app.get('/authors', async (_req, res) => {
   const authors = await author.getAll();
 
   res.status(200).json(authors);
-})
+});
+
+app.get('/authors/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const authorId = await author.findById(id);
+
+  if (!authorId) return res.status(404).json({ message: 'Not found '});
+
+  return res.status(200).json(authorId);
+});
 
 app.listen(PORT, () => {
   console.log(`Ouvindo porta ${PORT}`);
--- exercises/bloco_26/dia_1/conteudoAula/model_example/models/authors.js
+++ exercises/bloco_26/dia_1/conteudoAula/model_example/models/authors.js
@@ -2,13 +2,13 @@ const connection = require('./connection');
 
 // cria uma string com nome completo
 const getNewAuthor = (authorData) => {
-  const { id, first_name, middle_name, last_name } = authorData;
+  const { id, firstName, middleName, lastName } = authorData;
 
-  const fullName = [first_name, middle_name, last_name]
+  const fullName = [firstName, middleName, lastName]
     .filter((name) => name)
     .join(' ');
 
-  return { id, first_name, middle_name, last_name, name: fullName };
+  return { id, firstName, middleName, lastName, name: fullName };
 
 };
 
@@ -16,9 +16,9 @@ const getNewAuthor = (authorData) => {
 
 const serialize = (authorData) => ({
   id: authorData.id,
-  first_name: authorData.first_name,
-  middle_name: authorData.middle_name,
-  last_name: authorData.last_name,
+  firstName: authorData.first_name,
+  middleName: authorData.middle_name,
+  lastName: authorData.last_name,
 });
 
 // busca todos os autores do banco
@@ -30,4 +30,23 @@ const getAll = async () => {
   return authors.map(serialize).map(getNewAuthor);
 };
 
-module.exports = { getAll };
+// buscar autor por id
+const findById = async (id) => {
+  const query =
+    'SELECT id, first_name, middle_name, last_name FROM model_example.authors WHERE id=?';
+  
+  const [authorData] = await connection.execute(query, [id]);
+
+  if (authorData.length === 0) return null;
+
+  const { firstName, middleName, lastName} = serialize(authorData[0]);
+
+  return getNewAuthor({
+    id,
+    firstName,
+    middleName,
+    lastName,
+  });
+}
+
+module.exports = { getAll, findById };
