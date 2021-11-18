e47b270b2e88e134f1df0b478d035cd87d67a270
ArturAlvaro <artur.alvaro73@gmail.com>
2021-11-18 11:34:45 -0300

    Testando as camadas

---
+++ exercises/bloco_26/dia_4/movie/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const MovieController = require('./controllers/movieController');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.post('/movies', MovieController.create);
+
+app.listen(PORT, () => {
+  console.log(`Ouvindo a porta ${PORT}`);
+});
