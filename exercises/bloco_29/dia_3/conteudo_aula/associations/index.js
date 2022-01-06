c9aad70e987e949fadb7ba7096b734d958e16129
ArturAlvaro <artur.alvaro73@gmail.com>
2022-01-06 15:19:33 -0300

    Managed, unmanaged transactions

--- exercises/bloco_29/dia_3/conteudo_aula/associations/index.js
+++ exercises/bloco_29/dia_3/conteudo_aula/associations/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const Sequelize = require('sequelize');
 const { Address, Employee, Book, User } = require('./models');
+const config = require('./config/config');
 
 const app = express();
+app.use(bodyParser.json());
+
+const sequelize = new Sequelize(config.development);
 
 app.get('/employees', async (_req, res) => {
   try {
@@ -52,6 +58,55 @@ app.get('/usersbooks/:id', async (req, res) => {
     console.log(e.message);
     res.status(500).json({ message: 'Algo deu errado' });
   };
+});
+
+app.post('/employees', async (req, res) => {
+  try {
+    const { firstName, lastName, age, city, street, number } = req.body;
+
+    const employee = await Employee.create({ firstName, lastName, age });
+
+    await Address.create({ city, street, number, employeeId: employee.id });
+
+    return res.status(201).json({ message: 'Cadastrado com sucesso' });
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
+});
+
+app.post('/employees', async (req, res) => {
+  // Primeiro iniciamos a transação
+  const t = await sequelize.transaction();
+
+  try {
+    const { firstName, lastName, age, city, street, number } = req.body;
+
+    // Depois executamos as operações
+    const employee = await Employee.create(
+      { firstName, lastName, age },
+      { transaction: t },
+    );
+
+    await Address.create(
+      { city, street, number, employeeId: employee.id },
+      { transaction: t },
+    );
+
+    // Se chegou até essa linha, quer dizer que nenhum erro ocorreu.
+    // Com isso, podemos finalizar a transação usando a função `commit`.
+    await t.commit();
+
+    return res.status(201).json({ message: 'Cadastrado com sucesso' });
+    // caso não retorne nada, será uma unmanaged transaction
+  } catch (e) {
+    // Se entrou nesse bloco é porque alguma operação falhou.
+    // Nesse caso, o sequelize irá reverter as operações anteriores com a função rollback, não sendo necessário fazer manualmente
+    await t.rollback();
+    // mesma ideia, caso não tenha retorno, será unmanaged e o sequelize cuidará
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
 });
 
 const PORT = process.env.PORT || 3000;
