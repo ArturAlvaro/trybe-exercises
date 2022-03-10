7e87ac6daa2cee4b8e1b1ea1383be847c785825b
ArturAlvaro <artur.alvaro73@gmail.com>
2022-03-09 23:03:17 -0300

    exercises2

--- exercises/bloco_34/dia_1/exercises/test.py
+++ exercises/bloco_34/dia_1/exercises/test.py
@@ -1,23 +1,9 @@
 from parsel import Selector
 import requests
 
-URL_BASE = "http://books.toscrape.com/"
-response = requests.get(URL_BASE)
+URL_BASE = "https://en.wikipedia.org/wiki/List_of_programming_languages"
+response = requests.get("https://en.wikipedia.org/wiki/List_of_programming_languages")
 selector = Selector(text=response.text)
 
-href = selector.css(".product_pod .image_container a::attr(href)").get()
-img = selector.css(".product_pod .image_container a img::attr(src)").get()
-title = selector.css(".product_pod h3 a::attr(title)").get()
-price = selector.css(".product_pod .product_price .price_color").re_first(r"£\d+\.\d{2}")
-
-dict = {}
-book_link = URL_BASE + href
-detail_response = requests.get(book_link)
-detail_selector = Selector(text=detail_response.text)
-description = detail_selector.css("#product_description ~ p::text").get()
-
-suffix = "...more"
-if description.endswith(suffix):
-    description = description[:-len(suffix)]
-
-print(img)
+name = selector.css(".div_col ul li a::attr(title)").get()
+print(name)
